e740a2ed53435d1d236511d5c2b3620ad2ddee33
sriramvaidyanathan13 <85101772+sriramvaidyanathan13@users.noreply.github.com>
2025-03-25 08:25:40 +0000

    Update RunToscaTest.bat

--- .github/workflows/RunToscaTest.bat
+++ .github/workflows/RunToscaTest.bat
@@ -1,1 +1,2 @@
 java -jar ToscaCIJavaClient.jar -m distributed -c “c:/temp/TestConfigurationCIExecution.xml” -e “https://toscademovm:443/DistributionServerService/ManagerService.svc” -r “c:/temp/result.xml”
+
